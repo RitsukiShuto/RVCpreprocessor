71e0122138b101837c989cf852fe305839dbaeef
RitsukiShuto <rtk410s@gmail.com>
2023-06-17 00:09:06 +0900

    微修正

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # Created by RitsukiShuto on 2023/06/16.
 # 音声ファイルのノイズ除去、無音区間の除去を行う
 #
-import glob
-import sys
-
 import modules.splitWav as splitWav
 
 def main():
     # 音声ファイルを読み込む
     # 複数の音声ファイルを読み込む場合はglobを使用する
-    angeVoice = splitWav.splitWav()
+    angeVoice = splitWav.SplitWav()
 
     # 無音区間の除去を行う
     angeVoice.removeSilence()
